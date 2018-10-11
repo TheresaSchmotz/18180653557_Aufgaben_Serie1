b5986106f29f208bdb97d4b22b813ec91151637c
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-10-11 10:52:02 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String vorname = "Theresa";
+        int alter = (int)19.4;
+        System.out.println("Hello "+vorname +"You're" +alter);
+    }
+}
