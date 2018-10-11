6d9ef7aeebc77d26a026c7d0f7a3396b964eb5bc
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-10-11 11:31:23 +0200

    nur vor und nachname

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
         String nachname = "Schmotz";
         String vorname = "Theresa";
-        String wohnort = "Raubling";
-        int alter = 19;
-        int geburtsjahr = (int)1999;
-        System.out.println("Hello "+vorname +" " +nachname +". You're born in " +geburtsjahr +" and you are " +alter +" years old. And you are from " +wohnort +".");
+        System.out.println("Hello "+vorname +" " +nachname +".");
     }
 }
