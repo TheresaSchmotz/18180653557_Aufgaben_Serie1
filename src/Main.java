2e58b59e7489f3672efb6d22e591c7920b46a724
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-10-11 11:21:53 +0200

    alter,gebjahr, vor und nachname

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         String nachname = "Schmotz";
+        String vorname = "Theresa";
+        int alter = 19;
         int geburtsjahr = (int)1999;
-        System.out.println("Hello "+nachname +". You're born in " +geburtsjahr);
+        System.out.println("Hello "+vorname +" " +nachname +". You're born in " +geburtsjahr +" and you are " +alter +" years old.");
     }
 }
