74340a372748937833d4ebd842aa760e65870718
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-10-11 11:12:52 +0200

    Geburtsjahr und nachname geändert

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String vorname = "Theresa";
-        int alter = (int)19.4;
-        System.out.println("Hello "+vorname +"You're" +alter);
+        String nachname = "Schmotz";
+        int geburtsjahr = (int)1999;
+        System.out.println("Hello "+nachname +". You're born in " +geburtsjahr);
     }
 }
