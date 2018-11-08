da6db9f135b1c4a7a2501be34644c37f009ee4fe
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-11-08 10:43:25 +0100

    Hello World Kommentiert

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-public class Main {
+public class Main { // hier wir die Klasse "Main" erstellt
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Theresa Schmotz");
-        System.out.println();
+    public static void main(String[] args) { //hier wird die mainMethode, also der Startpunkt des Projektes erstellt. in der Methode wird erklärt was das Programm machen muss
+        System.out.println("Hello World"); //Das Programm soll "Hello World" ausgeben
+        System.out.println("Theresa Schmotz"); //Das Programm soll "Theresa Schmotz" ausgeben
     }
 }
