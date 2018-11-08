a8fb76f34778776edda49bd22113a127ba37a4f3
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-11-08 11:24:34 +0100

    baum

--- Package 3/src/Main3.java
+++ Package 3/src/Main3.java
@@ -1,14 +1,14 @@
-public class Main3
-{
-    public static void main(String[] args)
-    {
+public class Main3 {
+    public static void main(String[] args) {
         String name = "Theresa Schmotz";
         int alter = 19;
         final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
         /**Man hat einfach den Variablennamen(fachhochschuleKufsteinTirol -> fhKufsteinTirol) ausgestauscht;
-        variable FH_KUFSTEIN_TIROL ist jetzt final und kann zur laufzeit nichtmehr verändert werden.
-        */
+         variable FH_KUFSTEIN_TIROL ist jetzt final und kann zur laufzeit nichtmehr verändert werden.
+         */
         System.out.println(name);
         System.out.println(19);
+
+        //Aufgabe 5
+        final String Baum;
     }
-}
