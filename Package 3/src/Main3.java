bd6d7068cae17c30de20ad92c664896092431adc
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-11-08 11:02:41 +0100

    main 3 outprint name and alter

--- Package 3/src/Main3.java
+++ Package 3/src/Main3.java
@@ -3,6 +3,9 @@ public class Main3
     public static void main(String[] args)
     {
         String name = "Theresa Schmotz";
+        int alter = 19;
+
         System.out.println(name);
+        System.out.println(19);
     }
 }
