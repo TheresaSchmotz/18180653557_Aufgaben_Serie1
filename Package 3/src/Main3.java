8d1a6a8807f3e6522c43effa93127c1490bb1c37
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-11-08 11:11:15 +0100

    fhKufsteinTirol change

--- Package 3/src/Main3.java
+++ Package 3/src/Main3.java
@@ -4,6 +4,7 @@ public class Main3
     {
         String name = "Theresa Schmotz";
         int alter = 19;
+        String fhKufsteinTirol = "Fachhochschule Kufstein Tirol"; //Man hat einfach den Variablennamen(fachhochschuleKufsteinTirol -> fhKufsteinTirol) ausgestauscht
 
         System.out.println(name);
         System.out.println(19);
