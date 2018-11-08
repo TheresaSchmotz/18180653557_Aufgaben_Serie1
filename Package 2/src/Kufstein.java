a81197c61e5c45f62f99015d8c0b5780eddccae2
Schmotz Theresa <1810653557@stud.fh-kufstein.ac.at>
2018-11-08 10:47:33 +0100

    FH Kufstein Tirol new kommented

--- Package 2/src/Kufstein.java
+++
@@ -1,6 +0,0 @@
-public class Kufstein { // hier wir die Klasse "Kufstein" erstellt
-        public static void main(String[] args) //hier wird die mainMethode, also der Startpunkt des Projektes erstellt. in der Methode wird erklärt was das Programm machen muss
-        {
-            System.out.println("FH Kufstein Tirol"); //Das Programm soll "FH Kufstein Tirol" ausgeben
-        }
-    }
---
+++ Package 2/src/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 { // hier wir die Klasse "Main2" erstellt
+    public static void main(String[] args) //hier wird die mainMethode, also der Startpunkt des Projektes erstellt. in der Methode wird erklärt was das Programm machen muss
+    {
+        System.out.println("FH Kufstein Tirol"); //Das Programm soll "FH Kufstein Tirol" ausgeben
+    }
+}
